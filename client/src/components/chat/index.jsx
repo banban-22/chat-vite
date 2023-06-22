c2f598d75113a7e16af8115d08ae4c5432ee3966
AyakaB <ayaka.bando22@gmail.com>
2023-06-21 19:56:03 -0700

    created message form

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -1,28 +1,34 @@
-import React from 'react'
+import React from 'react';
 import {
-    useMultiChatLogic,
-    MultiChatSocket,
-    MultiChatWindow
-} from "react-chat-engine-advanced"
-import Header from "@/components/customHeader"
+  useMultiChatLogic,
+  MultiChatSocket,
+  MultiChatWindow,
+} from 'react-chat-engine-advanced';
+import Header from '@/components/customHeader';
+import StandardMessageForm from '@/components/customMessageForms/StandardMessageForm';
 
 const Chat = () => {
-    const chatProps = useMultiChatLogic(
-        import.meta.env.VITE_PROJECT_ID,
-        "testuser",
-        "123abc"
-    )
+  const chatProps = useMultiChatLogic(
+    import.meta.env.VITE_PROJECT_ID,
+    'testuser',
+    '123abc'
+  );
 
   return (
-    <div style={{ flexBasis: "100%" }}>
-        <MultiChatSocket {...chatProps} />
-        <MultiChatWindow 
-            {...chatProps}
-            style={{ height: "100vh"}}
-            renderChatHeader={(chat) => <Header chat={chat} />}
-        />
+    <div style={{ flexBasis: '100%' }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow
+        {...chatProps}
+        style={{ height: '100vh' }}
+        renderChatHeader={(chat) => <Header chat={chat} />}
+        renderMessageForm={(props) => {
+          return (
+            <StandardMessageForm props={props} activeChat={chatProps.chat} />
+          );
+        }}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
